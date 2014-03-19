b6966bba77346be068a68aa918cbafb19b7ae5f7
Chris Chang <chang.christopher@gmail.com>
2014-03-19 11:13:04 -0700

    -

--- nodejs/mongooseSimpleExample.js
+++ nodejs/mongooseSimpleExample.js
@@ -72,35 +72,35 @@ db.once('open', function callback () {
 
   Song.update({ song: 'One Sweet Day'}, { $set: { artist: 'Mariah Carey ft. Boyz II Men'} }, 
     function (err, numberAffected, raw) {
-        
-        if (err) return handleError(err);
 
-        /*
-         * Finally we run a query which returns all the hits that spend 10 or
-         * more weeks at number 1.
-         */
+      if (err) return handleError(err);
 
-        Song.find({ weeksAtOne: { $gte: 10} }).sort({ decade: 1}).exec(function (err, docs){
+      /*
+       * Finally we run a query which returns all the hits that spend 10 or
+       * more weeks at number 1.
+       */
 
-            if(err) throw err;
+      Song.find({ weeksAtOne: { $gte: 10} }).sort({ decade: 1}).exec(function (err, docs){
 
-            docs.forEach(function (doc) {
-                console.log(
-                  'In the ' + doc['decade'] + ', ' + doc['song'] + ' by ' + doc['artist'] + 
-                  ' topped the charts for ' + doc['weeksAtOne'] + ' straight weeks.'
-                );
-            });
+        if(err) throw err;
 
-            // Since this is an example, we'll clean up after ourselves.
-            mongoose.connection.db.collection('songs').drop(function (err) {
-              if(err) throw err;
-
-              // Only close the connection when your app is terminating
-              mongoose.connection.db.close(function (err) {
-                if(err) throw err;
-              });
-            });
+        docs.forEach(function (doc) {
+          console.log(
+            'In the ' + doc['decade'] + ', ' + doc['song'] + ' by ' + doc['artist'] + 
+            ' topped the charts for ' + doc['weeksAtOne'] + ' straight weeks.'
+          );
         });
+
+        // Since this is an example, we'll clean up after ourselves.
+        mongoose.connection.db.collection('songs').drop(function (err) {
+          if(err) throw err;
+
+          // Only close the connection when your app is terminating
+          mongoose.connection.db.close(function (err) {
+            if(err) throw err;
+          });
+        });
+      });
     }
   )
 });

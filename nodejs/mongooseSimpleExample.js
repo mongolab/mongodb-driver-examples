4cbf76e622157e27e9d2bd7b11aeecf77edd921b
Chris Chang <chang.christopher@gmail.com>
2014-04-03 10:48:05 -0700

    added connection settings

--- nodejs/mongooseSimpleExample.js
+++ nodejs/mongooseSimpleExample.js
@@ -9,7 +9,17 @@
 
 var mongoose = require('mongoose');
 var uriUtil = require('mongodb-uri');
-
+ 
+/* 
+ * Mongoose by default sets the auto_reconnect option to true.
+ * We recommend setting socket options at both the server and replica set level.
+ * We recommend a 30 second connection timeout because it allows for 
+ * plenty of time in most operating environments.
+ */
+ 
+var options = { server: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } }, 
+                replset: { socketOptions: { keepAlive: 1, connectTimeoutMS : 30000 } } };       
+ 
 /*
  * Mongoose uses a different connection string format than MongoDB's standard.
  * Use the mongodb-uri library to help you convert from the standard format to
@@ -17,7 +27,7 @@ var uriUtil = require('mongodb-uri');
  */
 
 var mongodbUri = 'mongodb://user:pass@host:port/db';
-var mongooseUri = uriUtil.formatMongoose(mongodbUri);
+var mongooseUri = uriUtil.formatMongoose(mongodbUri, options);
 
 mongoose.connect(mongooseUri);
 

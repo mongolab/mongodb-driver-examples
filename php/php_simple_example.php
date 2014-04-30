b6137e4f7f5e9f26a488f09e8c2bd527cf39abcf
Chris Chang <chang.christopher@gmail.com>
2014-04-30 15:39:08 -0700

    -

---
+++ php/php_simple_example.php
@@ -0,0 +1,86 @@
+<?php
+
+/*
+ * Copyright (c) 2014 ObjectLabs Corporation
+ * Distributed under the MIT license - http://opensource.org/licenses/MIT
+ *
+ * Written with extension mongo 1.4.3
+ * Documentation: http://php.net/mongo
+ * A PHP script connecting to a MongoDB database given a MongoDB Connection URI.
+ */
+
+// Create seed data
+
+$seedData = array(
+    array(
+        'decade' => '1970s', 
+        'artist' => 'Debby Boone',
+        'song' => 'You Light Up My Life', 
+        'weeksAtOne' => 10
+    ),
+    array(
+        'decade' => '1980s', 
+        'artist' => 'Olivia Newton-John',
+        'song' => 'Physical', 
+        'weeksAtOne' => 10
+    ),
+    array(
+        'decade' => '1990s', 
+        'artist' => 'Mariah Carey',
+        'song' => 'One Sweet Day', 
+        'weeksAtOne' => 16
+    ),
+);
+
+// Standard URI format: mongodb://[dbuser:dbpassword@]host:port/dbname
+
+$uri = "mongodb://user:pass@host:port/db";
+$client = new MongoClient($uri, array("connectTimeoutMS" => 30000);
+$db = $client->selectDB("dbname");
+
+/*
+ * First we'll add a few songs. Nothing is required to create the songs
+ * collection; it is created automatically when we insert.
+ */
+
+$songs = $db->songs;
+
+// To insert a dict, use the insert method.
+
+$songs->batchInsert($seedData);
+
+/*
+ * Then we need to give Boyz II Men credit for their contribution to
+ * the hit "One Sweet Day".
+*/
+
+$songs->update(
+    array('artist' => 'Mariah Carey'), 
+    array('$set' => array('artist' => 'Mariah Carey ft. Boyz II Men'))
+);
+    
+/*
+ * Finally we run a query which returns all the hits that spent 10 
+ * or more weeks at number 1. 
+*/
+
+$query = array('weeksAtOne' => array('$gte' => 10));
+$cursor = $songs->find($query)->sort(array('decade' => 1));
+
+foreach($cursor as $doc) {
+    echo 'In the ' .$doc['decade'];
+    echo ', ' .$doc['song']; 
+    echo ' by ' .$doc['artist'];
+    echo ' topped the charts for ' .$doc['weeksAtOne']; 
+    echo ' straight weeks.', "\n";
+}
+
+// Since this is an example, we'll clean up after ourselves.
+
+$songs->drop();
+
+// Only close the connection when your app is terminating
+
+$client->close();
+
+?>

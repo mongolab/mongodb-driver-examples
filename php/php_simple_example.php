4b682478048c1a2ce4c9f5bcf07881b5094a7082
Angela Kung Shulman <angela@objectlabs.com>
2014-05-28 11:49:00 -0700

    Update php_simple_example.php
    Formatting

--- php/php_simple_example.php
+++ php/php_simple_example.php
@@ -10,7 +10,6 @@
  */
 
 // Create seed data
-
 $seedData = array(
     array(
         'decade' => '1970s', 
@@ -36,7 +35,6 @@ $seedData = array(
  * Standard single-node URI format: 
  * mongodb://[username:password@]host:port/[database]
  */
-
 $uri = "mongodb://myuser:mypass@host:port/mydb";
 $options = array("connectTimeoutMS" => 30000);
 
@@ -45,7 +43,6 @@ $options = array("connectTimeoutMS" => 30000);
  *   $uri = "mongodb://myuser:mypass@host1:port1,host2:port:2/mydb";
  *   $options = array("replicaSet" => "myReplicaSet", "connectTimeoutMS" => 30000);
  */
-
 $client = new MongoClient($uri, $options );
 $db = $client->selectDB("mydb");
 
@@ -64,7 +61,6 @@ $songs->batchInsert($seedData);
  * Then we need to give Boyz II Men credit for their contribution to
  * the hit "One Sweet Day".
 */
-
 $songs->update(
     array('artist' => 'Mariah Carey'), 
     array('$set' => array('artist' => 'Mariah Carey ft. Boyz II Men'))
@@ -74,7 +70,6 @@ $songs->update(
  * Finally we run a query which returns all the hits that spent 10 
  * or more weeks at number 1. 
 */
-
 $query = array('weeksAtOne' => array('$gte' => 10));
 $cursor = $songs->find($query)->sort(array('decade' => 1));
 
@@ -87,11 +82,9 @@ foreach($cursor as $doc) {
 }
 
 // Since this is an example, we'll clean up after ourselves.
-
 $songs->drop();
 
 // Only close the connection when your app is terminating
-
 $client->close();
 
 ?>

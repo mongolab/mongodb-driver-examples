d5db794e363c87f4c1a07bed5862c3bc64857d8d
Nathan Park <nathan@objectlabs.com>
2017-02-01 17:09:59 -0800

    update copyright and documentation link

--- c#/CSharpSimpleExample.cs
+++ c#/CSharpSimpleExample.cs
@@ -1,9 +1,9 @@
 /*
- * Copyright (c) 2016 ObjectLabs Corporation
+ * Copyright (c) 2017 ObjectLabs Corporation
  * Distributed under the MIT license - http://opensource.org/licenses/MIT
  *
- * Written with CSharpDriver-2.2.0
- * Documentation: http://api.mongodb.org/csharp/
+ * Written with CSharpDriver-2.4.1
+ * Documentation: http://docs.mongodb.org/ecosystem/drivers/csharp/
  * A C# class connecting to a MongoDB database given a MongoDB Connection URI.
  */
 

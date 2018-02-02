978ca8bef5cf51a54704773e528e9019f0497a27
Tim Fogarty <timothy.fogo@gmail.com>
2018-02-02 13:54:19 -0800

    Fix some formatting and whitespace

--- nodejs/mongooseSimpleExample.js
+++ nodejs/mongooseSimpleExample.js
@@ -17,7 +17,7 @@ let db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 
 db.once('open', function callback() {
-  
+
   // Create song schema
   let songSchema = mongoose.Schema({
     decade: String,
@@ -55,49 +55,50 @@ db.once('open', function callback() {
    * First we'll add a few songs. Nothing is required to create the
    * songs collection; it is created automatically when we insert.
    */
+
   let list = [seventies, eighties, nineties]
-  
+
   Song.insertMany(list).then(() => {
-    
+
     /*
      * Then we need to give Boyz II Men credit for their contribution
      * to the hit "One Sweet Day".
      */
-     
+
     return Song.update({ song: 'One Sweet Day'}, { $set: { artist: 'Mariah Carey ft. Boyz II Men'} })
-    
+
   }).then(() => {
-    
+
     /*
      * Finally we run a query which returns all the hits that spend 10 or
      * more weeks at number 1.
      */
-    
+
     return Song.find({ weeksAtOne: { $gte: 10} }).sort({ decade: 1})
-    
+
   }).then(docs => {
-    
+
     docs.forEach(doc => {
       console.log(
         'In the ' + doc['decade'] + ', ' + doc['song'] + ' by ' + doc['artist'] +
         ' topped the charts for ' + doc['weeksAtOne'] + ' straight weeks.'
       );
     });
-    
+
   }).then(() => {
-    
+
     // Since this is an example, we'll clean up after ourselves.
     return mongoose.connection.db.collection('songs').drop()
-    
+
   }).then(() => {
-    
+
     // Only close the connection when your app is terminating
     return mongoose.connection.close()
-    
+
   }).catch(err => {
-    
+
     // Log any errors that are thrown in the Promise chain
     console.log(err)
-    
+
   })
 });
